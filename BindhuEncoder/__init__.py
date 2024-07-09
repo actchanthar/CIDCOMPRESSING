d4d4c2c685b29df2a450dddcb1c0be80b3be4e2f
actchanthar <159926023+actchanthar@users.noreply.github.com>
2024-07-09 16:23:43 +0630

    Update __init__.py

--- BindhuEncoder/__init__.py
+++ BindhuEncoder/__init__.py
@@ -14,7 +14,7 @@ if os.path.exists('BindhuEncoder/config.env'):
 
 # Variables
 
-api_id = int(os.environ.get("API_ID")
+api_id = int(os.environ.get("API_ID", "28090485"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 
